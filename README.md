0e826a148f0b153dc495e96edfb77d5d10867dcc
andres naranjo <afnarqui@hotmail.com>
2016-05-25 23:42:37 -0500

    explicando callbacks

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 nodejs v6
 https://nodejs.org/api/
 npm init --yes
-npm install babel-preset-es2015 --save-dev
-npm install --save-dev babel-cli
-npm install --save-dev babel-polyfill
-npm install --save-dev babelify
+/*
+serve
+  "dependencies": {
+    "serve": "^1.4.0"
+  },
+  "devDependencies": {
+    "babel-preset-es2015": "^6.1.18",
+    "babelify": "^7.2.0",
+    "browserify": "^12.0.1"
+  },
+  "scripts": {
+    "public": "mkdir -p public",
+    "build-js": "browserify -t [ babelify --presets [ es2015 ] ] app.js > public/app.js",
+    "serve": "serve public",
+    "copy-files": "copy src/index.css public/app.css && cp src/index.html public/index.html",
+    "build": "npm run public && npm run build-js && npm run copy-files",
+  }
+  */
+  
+
--- app.js
+++ app.js
@@ -5,10 +5,21 @@ var fs = require('fs')
 utilizando el módulo fs de node para explicar los metodos async 
 devuelve una representación del archivo 
 datos esta en una representación de tipo buffer y es un archivo no leible por un ser humano
-*/
-let datos = fs.readFileSync('index.html')
 
-console.log(datos.toString())
+las funciones sincrónicas lo que hacen es bloquear el proceso hasta que la función termine de
+conseguir el archivo en el disco rigido y lo devuelva en la variable datos
+a hoy todos los lenguajes de programación funcionan así. pero nodejs no
+
+ejecutando el archivo de forma asíncrona
+callback ()=>{} es una función que se va a ejecutar cuando el archivo se termine de leer 
+readFile le pasa al callback el contenido del archivo
+*/
+fs.readFile('index.html',(err,data)=>{
+    if(err){
+        return console.log(`Se presento un error al leer el archivo : ${err}`)
+    }
+    console.log(data.toString().length)    
+})
 
 console.log('esto es un mensaje')
 
